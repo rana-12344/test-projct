a1cea13977fc2782db158e262a03cb7ec9931723
Taif <74107163+Taif50@users.noreply.github.com>
2020-11-08 13:36:04 +0300

    Update JavaApplication14.java

--- JavaApplication14.java
+++ JavaApplication14.java
@@ -11,16 +11,16 @@ public class JavaApplication14 {
     public static void main(String[] args) {
         ArrayList<Integer> p= new ArrayList <>();
         int sum=0;
-        for ( int i=1; i<=10;i++){
-            p.add(i);
-            sum+=i;
+        for ( int i=1; i<=10;i++) {
+            p.add(i) ;
+            sum+=i ;
         }
-        System.out.println(p);
-        System.out.println(sum);
-        p.remove(9);
-        System.out.println(p);
-         p.add(70);
-          System.out.println(p);
+        System.out.println(p) ;
+        System.out.println(sum) ;
+        p.remove(9) ;
+        System.out.println(p) ;
+         p.add(70) ;
+          System.out.println(p) ;
         // TODO code application logic here
     }
     
