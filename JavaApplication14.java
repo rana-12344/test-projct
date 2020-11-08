3d9f36f872bde6c7c9a1460471a962d0b2f8e047
rana-12344 <74002474+rana-12344@users.noreply.github.com>
2020-11-07 19:38:29 -0800

    Update JavaApplication14.java

--- JavaApplication14.java
+++ JavaApplication14.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication14;
 import java.util.ArrayList;
 
-/**
- *
- * @author lenovo
- */
+
 public class JavaApplication14 {
 
     /**
